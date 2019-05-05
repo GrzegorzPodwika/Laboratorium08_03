98fce6fc6f0ce987d6376dde3077692777725c93
Grzegorz Podwika <podwikagrzegorz@gmail.com>
2019-05-05 18:12:13 +0200

    Dokonanie koncowych zmian w algorytmie rekursywnym i koncowe okomentowanie.

--- lab08_03/knapsackProblemByRecursion.cpp
+++ lab08_03/knapsackProblemByRecursion.cpp
@@ -2,44 +2,86 @@
 
 using namespace std;
 
-void showResultsOnScreen()
+void manageAndShowResultsOnScreen()
 {
 	fstream inFile = createInputFile();
 
 	//pomocnicze zmienne
 	int numberOfDataSets{};
 	int capacityOfKnapsack{}, numberOfItems{};
-	int *weightsOfItems{}, *resTab;
+	int *weightsOfItems{};
+	vector <int> resVec;
 
-	inFile >> numberOfDataSets;
+	try
+	{
+		inFile >> numberOfDataSets;
+		if (numberOfDataSets < 0)
+			throw EXCEPTION_CODE::notNaturalNumber;
+	}
+	catch (EXCEPTION_CODE error)
+	{
+		seeExceptions(error);
+	}
+
 
 	for (int i = 0; i < numberOfDataSets; i++)
 	{
-		inFile >> capacityOfKnapsack >> numberOfItems;
+		try
+		{
+			inFile >> capacityOfKnapsack >> numberOfItems;
+			if (capacityOfKnapsack < 0 || numberOfItems < 0)
+				throw EXCEPTION_CODE::notNaturalNumber;
+		}
+		catch (EXCEPTION_CODE error)
+		{
+			seeExceptions(error);
+		}
 
 		weightsOfItems = new int[numberOfItems];
-		resTab = new int[numberOfItems];
 
 		for (int j = 0; j < numberOfItems; j++)
 		{
-			inFile >> weightsOfItems[j];
+			try
+			{
+				inFile >> weightsOfItems[j];
+				if (weightsOfItems[j])
+					throw EXCEPTION_CODE::notNaturalNumber;
+			}
+			catch (EXCEPTION_CODE error)
+			{
+				seeExceptions(error);
+			}
 		}
 
-		cout << rekurencja(weightsOfItems, numberOfItems, capacityOfKnapsack) << endl;
+		if (isCapacityOfKnapsackEnough(weightsOfItems, resVec, numberOfItems, capacityOfKnapsack))
+		{
+			cout << capacityOfKnapsack << " = ";
+			for (int x : resVec)
+			{
+				cout << x << " ";
+			}
+			cout << endl;
+		}
+		else
+		{
+			cout << "BRAK" << endl;
+		}
 
+		resVec.clear();
 		delete[] weightsOfItems;
-		delete[] resTab;
 	}
 
 }
 
-int rekurencja(int *weightsOfItems, int sizeOfTOW, int maxCapacity, int sumOfElem, int begIterator, int midIterator, int endIterator)
+bool isCapacityOfKnapsackEnough(int *weightsOfItems, vector <int> &resVec, int sizeOfTOW, int maxCapacity, int sumOfElem, int begIterator, int midIterator, int endIterator)
 {
+	//dokonczyc z resTab
+
 	if (sumOfElem == maxCapacity)
-		return 1;
+		return true;
 	else if (begIterator == sizeOfTOW)
 	{
-		return -1;
+		return false;
 	}
 
 	if (sumOfElem > maxCapacity)
@@ -49,30 +91,38 @@ int rekurencja(int *weightsOfItems, int sizeOfTOW, int maxCapacity, int sumOfEle
 		{
 		begIterator++;
 
+		resVec.clear();
 		sumOfElem = 0;
 		midIterator = 1;
 		endIterator = begIterator;
 
-		return rekurencja(weightsOfItems, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
+		return isCapacityOfKnapsackEnough(weightsOfItems, resVec, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
 		}
 
 		if (endIterator < sizeOfTOW)
 		{
 		sumOfElem -= weightsOfItems[endIterator - 1];
 		sumOfElem += weightsOfItems[endIterator];
+
+		resVec.pop_back();
+		resVec.push_back(weightsOfItems[endIterator]);
+
 		endIterator++;
 
-		return rekurencja(weightsOfItems, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
+		return isCapacityOfKnapsackEnough(weightsOfItems, resVec, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
 		}
 		else if (endIterator == sizeOfTOW)
 		{
 		sumOfElem = 0;
 		sumOfElem += weightsOfItems[begIterator];
 
+		resVec.clear();
+		resVec.push_back(weightsOfItems[begIterator]);
+
 		midIterator++;
 		endIterator = endIterator - sizeOfTOW + midIterator;
 
-		return rekurencja(weightsOfItems, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
+		return isCapacityOfKnapsackEnough(weightsOfItems, resVec, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
 		}
 
 
@@ -84,29 +134,34 @@ int rekurencja(int *weightsOfItems, int sizeOfTOW, int maxCapacity, int sumOfEle
 		{
 			begIterator++;
 
+			resVec.clear();
 			sumOfElem = 0;
 			midIterator = 1;
 			endIterator = begIterator;
 
-			return rekurencja(weightsOfItems, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
+			return isCapacityOfKnapsackEnough(weightsOfItems, resVec, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
 		}
 
 		if (endIterator < sizeOfTOW) {
 			sumOfElem += weightsOfItems[endIterator];
+			resVec.push_back(weightsOfItems[endIterator]);
+
 			endIterator++;
 
-
-			return rekurencja(weightsOfItems, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
+			return isCapacityOfKnapsackEnough(weightsOfItems, resVec, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
 		}
 		else if (endIterator == sizeOfTOW)
 		{
 			sumOfElem = 0;
 			sumOfElem += weightsOfItems[begIterator];
 
+			resVec.clear();
+			resVec.push_back(weightsOfItems[begIterator]);
+
 			midIterator++;
 			endIterator = endIterator - sizeOfTOW + midIterator;
 
-			return rekurencja(weightsOfItems, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
+			return isCapacityOfKnapsackEnough(weightsOfItems, resVec, sizeOfTOW, maxCapacity, sumOfElem, begIterator, midIterator, endIterator);
 		}
 	}
 
--- lab08_03/knapsackProblemByRecursion.h
+++ lab08_03/knapsackProblemByRecursion.h
@@ -2,11 +2,28 @@
 #define KNAPSACKPROBLEMBYRECURSION_H
 
 #include "libFiles.h"
+#include "libExceptions.h"
 #include <iostream>
+#include <vector>
 
-void showResultsOnScreen();
+/*
+Glowna funkcja zarzadzajaca i pokazujaca wyniki na ekran
+*/
+void manageAndShowResultsOnScreen();
 
-int rekurencja(int *weightsOfItems, int sizeOfTOW, int maxCapacity, int sumOfElem = 0, int begIterator = 0, int midIterator = 1, int endIterator = 0);
+/*
+Funkcja rekurencyjna obliczajaca czy na dana pojemnosc plecaka zmieszcza sie poszczegolne rzeczy
+@param weightsOfItems wagi p9oszczegolnych rzeczy pobranych z pliku tekstowego
+@param resVec wektor wynikowych wartosci, jesli upakowanie sie powiedzie
+@param sizeOfTOW rozmiar tablicy weightsOfItems
+@param maxCapacity maksymalnma pojemnosc danego plecaka
+@param sumOfElem suma elementow potrzebna do obliczen, domyslnie startowo = 0
+@param begIterator iterator wskazujacy na poczatek tablicy weightsOfItems, potrzebny do obliczen, startowo = 0
+@param midIterator iterator srodkowy na tablice weightsOfItems, potrzebny do obliczen, startowo = 1
+@param endIterator iterator przemieszczajacy sie po poszczegolnych elementach do koncowego elementu, potrzebny do obliczen, startowo = 0
+@return true jesli mozna dokladnie upakowac plecak z poszczegolnych rzeczy, czyli maja mase maxCapacity. False w przeciwnym wypadku
+*/
+bool isCapacityOfKnapsackEnough(int *weightsOfItems, std::vector <int> &resVec, int sizeOfTOW, int maxCapacity, int sumOfElem = 0, int begIterator = 0, int midIterator = 1, int endIterator = 0);
 
 
 
